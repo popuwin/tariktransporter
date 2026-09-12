741236f431b6823f9ab661bc052a124c87aa1270
LLL112341 <dillarionov017@gmail.com>
2026-09-13 01:30:30 +0400

    Add backend/src/main/java/com/tarikperevozki/TarikPerevozkiApplication.java

---
+++ backend/src/main/java/com/tarikperevozki/TarikPerevozkiApplication.java
@@ -0,0 +1,11 @@
+package com.tarikperevozki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarikPerevozkiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TarikPerevozkiApplication.class, args);
+    }
+}
